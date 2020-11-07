973129ee31c04f20faa4dadc3cb6bcd4b907867c
steve_global <18079940168@163.com>
2020-11-08 00:20:24 +0800

    打个招呼

---
+++ hello_git.py
@@ -0,0 +1,8 @@
+# D:\Users\Administrator\Anaconda3\python.exe
+# -*- coding: UTF-8 -*-
+# @Author  : Steve
+# @File    : hello_git.py
+# @Software: PyCharm
+# @Time    : 2020-11-08 上午 12:16
+
+print("Hello Git")
